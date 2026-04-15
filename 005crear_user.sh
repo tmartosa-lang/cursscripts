f7e76b392ef1c02711be66381a66ecd1faf015c6
tony <tmartosa@esliceu.net>
2026-04-15 16:06:38 +0200

    sexte commit

--- 005crear_user.sh
+++ 005crear_user.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+function usage(){
+    echo "Usage ${0}"
+    echo
+    echo "has de ser root"
+    echo "l'usuari no és pot repetir"
+    exit 1
+}
 
 #Script per crear un usuari 
 
 #primer comprovam si som l'usuari root
 if [[ ${UID} -ne 0 ]]
 then
-    echo "Usage ${0}"
-    echo
-    echo "has de ser root"
-    exit 1
+    usage
 fi
 
 read -p "Introdueix el nom d'usuari: " USER_NAME
 
-echo "creant l'usuari ${USER_NAME}"
+echo "creant l'usuari ${USER_NAME}"                                                                                                     
