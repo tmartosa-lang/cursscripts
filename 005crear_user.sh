85a9275084e113fe4f73f896ed8e9c5962afb15a
tony <tmartosa@esliceu.net>
2026-04-15 16:44:15 +0200

    quint commit

--- 005crear_user.sh
+++ 005crear_user.sh
@@ -4,7 +4,7 @@ function usage(){
     echo
     echo "has de ser root"
     echo "l'usuari no és pot repetir"
-    exit 1
+    exit 1 #indica que hi ha hagut una errada
 }
 
 #Script per crear un usuari 
@@ -15,6 +15,32 @@ then
     usage
 fi
 
+read -p "Introdueix el nom complet: " COMMENTS
+#demanam el nom de l'usuari
 read -p "Introdueix el nom d'usuari: " USER_NAME
+#demanam el password
+read -p "Introdueix  el password: " PASSWORD
 
-echo "creant l'usuari ${USER_NAME}"                                                                                                     
+
+#crearíem l'usuari
+useradd -m -c "${COMMENTS}" ${USER_NAME} &> /dev/null
+#control de la darrera comanda
+if [[ ${?} -ne 0 ]]
+then
+    echo "Errada creant l'usuari"
+    exit 1
+fi
+
+#canvi de password
+echo "${USER_NAME}:${PASSWORD}" | chpasswd
+#comprovam si el canvi de password ha anat bé.
+if [[ ${?} -ne 0 ]]
+then
+    echo "Errada canviant password"
+    exit 1
+fi
+
+#fer caducar el password
+passwd -e ${USER_NAME}
+
+exit 0
