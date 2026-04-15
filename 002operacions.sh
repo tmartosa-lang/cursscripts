9fda0ef743563c1697c5a9985ac240c0725287f6
tony <tmartosa@esliceu.net>
2026-04-15 15:15:42 +0200

    primeros scripts

---
+++ 002operacions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#variables són "paraules" que contenen un valor
+NOM="tony"
+#read serveix per llegir informació del teclt
+read -p "introdueix un nom: " NOM
+
+echo "El teu nom és ${NOM}"
---
+++ 003aritmetica.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#aquest script ens mostra com
+#realitzar les principals operacions aritmétiques
+
+read -p "introdueix el valor de X: " X
+read -p "introdueix el valor de Y: " Y
+
+echo "Has introduït ${X} i ${Y}"
+SUMA=$((X+Y))
+echo "${X}+${Y}=${SUMA}"
+RESTA=$((X-Y))
+echo "${X}-${Y}=${RESTA}"
+PRODUCTE=$((X*Y))
+echo "${X}*${Y}=${PRODUCTE}"
+DIVISIO=$(echo "scale=2; ${X}/${Y}" | bc)
+echo "${X}/${Y}=${DIVISIO}"
