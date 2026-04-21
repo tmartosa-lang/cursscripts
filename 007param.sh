613c57da014c6c039d5715497e66e68c44c9252b
tony <tmartosa@esliceu.net>
2026-04-21 16:54:39 +0200

    Video nº18 - Comanda Shift

--- 007param.sh
+++ 007param.sh
@@ -19,4 +19,8 @@ echo "Nom del script ${0}"
 echo "primer paràmetre: ${1}"
 USER_NAME=${1}
 echo "USER_NAME: ${USER_NAME}"
-echo "segon paràmetre: ${2}"
+#desplaçar els paràmetres a l'esquerra
+shift
+
+NOM_COMPLET=${*}
+echo "NOM_COMPLET: ${NOM_COMPLET}"
